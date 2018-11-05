1048ba7a708955bd6ec42f2a5aa566d86d826890
snake-cache <1287414150@qq.com>
2018-11-05 14:13:48 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: path.resolve(__dirname, './src/main.js'),
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				exclude: '/node_modules/'
+			}
+		]
+	}
+}
